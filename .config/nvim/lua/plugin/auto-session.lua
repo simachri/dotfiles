81be38c2508ed4928a45b501d022c2716f528281
Marvin Christe <marvin.christe@posteo.de>
2021-07-31 16:03:06 +0200

    feat(nvim-session): New mapping <leader>rs to RestoreSession.

--- .config/nvim/lua/plugin/auto-session.lua
+++ .config/nvim/lua/plugin/auto-session.lua
@@ -1,9 +1,13 @@
+-- https://github.com/rmagatti/auto-session
+-- 21-07-31: The key remapping is required to suppress
+-- a 'bwipe 1' error when restoring a session.
+vim.api.nvim_set_keymap("n", "<Leader>rs", "<cmd>silent! RestoreSession<CR>", {})
 local opts = {
   log_level = 'info',
   auto_session_enable_last_session = false,
-  auto_session_root_dir = "~/.config/nvim/sessions/",
-  auto_session_enabled = true,
-  auto_save_enabled = nil,
+  auto_session_root_dir = "/home/xi3k/.config/nvim/sessions/",
+  auto_session_enabled = false,
+  auto_save_enabled = true,
   auto_restore_enabled = nil,
   auto_session_suppress_dirs = nil
 }
