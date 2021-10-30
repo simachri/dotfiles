419fb436f1cf1e180501f69fc8839a68216eb46f
Marvin Christe <marvin.christe@posteo.de>
2021-10-30 10:19:28 +0200

    feat(Telescope): Color dev icons.

--- .config/nvim/lua/plugin/telescope.lua
+++ .config/nvim/lua/plugin/telescope.lua
@@ -38,7 +38,7 @@ require('telescope').setup{
     winblend = 0,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-    color_devicons = false,
+    color_devicons = true,
     use_less = true,
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
