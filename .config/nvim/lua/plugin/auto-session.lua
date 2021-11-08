723398151f981d8aab8665df36b090705cac3241
simachri <marvin.christe@posteo.de>
2021-11-08 19:33:33 +0100

    fix(autosession): 'scrolloff' is set to '0'.

--- .config/nvim/lua/plugin/auto-session.lua
+++ .config/nvim/lua/plugin/auto-session.lua
@@ -8,7 +8,9 @@ local opts = {
   auto_session_root_dir = "/home/xi3k/.config/nvim/sessions/",
   auto_session_enabled = true,
   auto_save_enabled = true,
-  auto_restore_enabled = true,
-  auto_session_suppress_dirs = nil
+  auto_restore_enabled = false,
+  auto_session_suppress_dirs = nil,
+  -- For some reason, scrolloff is set to 0 when a session is restored.
+  post_restore_cmds = {"set scrolloff=8"}
 }
 require('auto-session').setup(opts)
