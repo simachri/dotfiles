0e27237e2a6cbbeff8b81014d025bb62ff6fc236
simachri <marvin.christe@posteo.de>
2021-11-24 04:30:57 +0100

    feat(harpoon): Add mapping to jump to 4. entry.

--- .config/nvim/lua/plugin/harpoon.lua
+++ .config/nvim/lua/plugin/harpoon.lua
@@ -16,7 +16,8 @@ require("harpoon").setup({
 vim.api.nvim_set_keymap("n", "<Leader>hh", "<Cmd>lua require('harpoon.mark').add_file()<CR>", { noremap = true, silent = true })
 -- Show marks
 vim.api.nvim_set_keymap("n", "<Leader>ht", "<Cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { noremap = true, silent = true })
--- Jump to mark 1 - 3
+-- Jump to mark 1 - 4
 vim.api.nvim_set_keymap("n", "<Leader>ha", "<Cmd>lua require('harpoon.ui').nav_file(1)<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>hs", "<Cmd>lua require('harpoon.ui').nav_file(2)<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>hd", "<Cmd>lua require('harpoon.ui').nav_file(3)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>hf", "<Cmd>lua require('harpoon.ui').nav_file(4)<CR>", { noremap = true, silent = true })
