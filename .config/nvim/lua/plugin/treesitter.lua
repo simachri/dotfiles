6a2c35a5d1e7f7102c9de4370fe7a55c2f473e1d
simachri <marvin.christe@posteo.de>
2022-01-14 08:21:42 +0100

    feat(treesitter): Disable markdown

--- .config/nvim/lua/plugin/treesitter.lua
+++ .config/nvim/lua/plugin/treesitter.lua
@@ -1,10 +1,12 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 require'nvim-treesitter.configs'.setup {
    --ensure_installed = {'python', 'go', 'lua', 'javascript', 'typescript', 'css' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-   -- 21-12-21: Use markdown once "jump to anchor" is implemented.
   ensure_installed = {'python', 'go', 'lua', 'javascript', 'typescript', 'css', 'markdown'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
+   -- 22-1-4: Enabling 'markdown' is slow on large files. Further investigation is
+   -- required.
+    disable = {"markdown"}
   },
   indent = {
     enable = true,              -- false will disable the whole extension
