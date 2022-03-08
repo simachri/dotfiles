420118148b4ea3463a0eb396cd63327f281a0c48
simachri <marvin.christe@posteo.de>
2022-03-08 14:50:47 +0100

    fix(LSP): null-ls also attaches to other filetypes than JS/TS.

--- .config/nvim/lua/plugin/nvim-lspconfig.lua
+++ .config/nvim/lua/plugin/nvim-lspconfig.lua
@@ -225,9 +225,15 @@ nvim_lsp.tsserver.setup({
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.formatting.prettier
+        null_ls.builtins.diagnostics.eslint_d.with({
+          filetypes = { "javascript", "typescript" },
+        }),
+        null_ls.builtins.code_actions.eslint_d.with({
+          filetypes = { "javascript", "typescript" },
+        }),
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = { "javascript", "typescript" },
+        }),
     },
     on_attach = on_attach
 })
