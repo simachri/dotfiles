6f8f8404a61c21f37b429bbc2e0f19a142f02282
simachri <marvin.christe@posteo.de>
2022-03-27 06:15:41 +0200

    LuaSnip: Use new definition format.

--- .config/nvim/lua/plugin/luasnip.lua
+++ .config/nvim/lua/plugin/luasnip.lua
@@ -35,9 +35,22 @@ ls.config.set_config({
         -- history: If true, Snippets that were exited can still be jumped back into. As
         -- Snippets are not removed when their text is deleted, they have to be removed
         -- manually via LuasnipUnlinkCurrent.
-        history = true
+        history = true,
+	---- Update more often, :h events for more info.
+	--updateevents = "TextChanged,TextChangedI",
+	--ext_opts = {
+		--[types.choiceNode] = {
+			--active = {
+				--virt_text = { { "choiceNode", "Comment" } },
+			--},
+		--},
+	--},
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 300,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	-- enable_autosnippets = true,
 })
-
 local function splitPath(path)
   local elems = {}
   --vim.api.nvim_echo({{path, "WarningMsg"}}, true, {}) -- default {history = true}
@@ -49,111 +62,110 @@ local function splitPath(path)
   return elems
 end
 
-ls.snippets = {
-    all={},
-    markdown = {
-            -- Checkbox: "- [ ]", trigger is 'cb'.
-            s({trig="cb"}, {
-                    t({"- [ ] "}),
-                    i(0),
-            }),
-            -- URL in the format [<URL name>](<URL>), trigger is 'url'.
-            s({trig="url"}, {
-                    t({"["}),
-                    -- Placeholder with initial text.
-                    i(1, {"<link name>"}),
-                    t({"]("}),
-                    f(function()
-                        return {vim.api.nvim_eval("@+")}
-                      end, {}),
+ls.add_snippets("markdown",
+                {
+                  -- Checkbox: "- [ ]", trigger is 'cb'.
+                  s({trig="cb", name="Insert checkbox"}, {
+                          t({"- [ ] "}),
+                          i(0),
+                  }),
+                  -- URL in the format [<URL name>](<URL>), trigger is 'url'.
+                  s({trig="url", name="Insert external link"}, {
+                          t({"["}),
+                          -- Placeholder with initial text.
+                          i(1, {"<link name>"}),
+                          t({"]("}),
+                          f(function()
+                              return {vim.api.nvim_eval("@+")}
+                            end, {}),
 
-                    -- Last Placeholder, exit Point of the snippet. EVERY 'outer' SNIPPET NEEDS Placeholder 0.
-                    i(0),
-                    t({")"}),
-            }),
-            -- trigger is 'ref'.
-            s({trig="ref"}, {
-                    t({"["}),
-                    -- Placeholder with initial text.
-                    i(1, {"<link name>"}),
-                    t({"]("}),
-                    f(function()
-                        local destPath = vim.api.nvim_eval("@x")
-                        local currFileParentDirPath = vim.api.nvim_eval("expand('%:h')")
-                        local anchor = vim.api.nvim_eval("@y")
-                        local function starts_with(entireStr, startStr)
-                            return entireStr:sub(1,#startStr) == startStr
-                        end
-                        -- Example:
-                        -- destPath = SAP/ECTR/Documents/Solutions
-                        -- currFileParentDirPath = SAP/ECTR/CAD/Cloning/API
-                        -- Result: ../../CAD/Cloning/API
+                          -- Last Placeholder, exit Point of the snippet. EVERY 'outer' SNIPPET NEEDS Placeholder 0.
+                          i(0),
+                          t({")"}),
+                  }),
+                  -- trigger is 'ref'.
+                  s({trig="ref", name="Insert reference link"}, {
+                          t({"["}),
+                          -- Placeholder with initial text.
+                          i(1, {"<link name>"}),
+                          t({"]("}),
+                          f(function()
+                              local destPath = vim.api.nvim_eval("@x")
+                              local currFileParentDirPath = vim.api.nvim_eval("expand('%:h')")
+                              local anchor = vim.api.nvim_eval("@y")
+                              local function starts_with(entireStr, startStr)
+                                  return entireStr:sub(1,#startStr) == startStr
+                              end
+                              -- Example:
+                              -- destPath = SAP/ECTR/Documents/Solutions
+                              -- currFileParentDirPath = SAP/ECTR/CAD/Cloning/API
+                              -- Result: ../../CAD/Cloning/API
 
-                        --vim.api.nvim_echo({{destPath, "WarningMsg"}}, true, {}) -- default {history = true}
-                        --vim.api.nvim_echo({{currFileParentDirPath, "WarningMsg"}}, true, {}) -- default {history = true}
+                              --vim.api.nvim_echo({{destPath, "WarningMsg"}}, true, {}) -- default {history = true}
+                              --vim.api.nvim_echo({{currFileParentDirPath, "WarningMsg"}}, true, {}) -- default {history = true}
 
-                        -- Remove any leading './'
-                        if starts_with(destPath, './') then destPath = string.sub(destPath, 3, #destPath) end
-                        if starts_with(currFileParentDirPath, './') then currFileParentDirPath = string.sub(currFileParentDirPath, 3, #currFileParentDirPath) end
+                              -- Remove any leading './'
+                              if starts_with(destPath, './') then destPath = string.sub(destPath, 3, #destPath) end
+                              if starts_with(currFileParentDirPath, './') then currFileParentDirPath = string.sub(currFileParentDirPath, 3, #currFileParentDirPath) end
 
-                         -- Split the paths into their directories.
-                        local destPathElems = splitPath(destPath)
-                        local currFilePathElems = splitPath(currFileParentDirPath)
-                        -- Identify the first elements of the path that are identical.
-                        local branchIdx = 0
-                        for idx, val in ipairs(destPathElems) do
-                          --vim.api.nvim_echo({{val, "WarningMsg"}}, true, {}) -- default {history = true}
-                          --vim.api.nvim_echo({{currFilePathElems[idx], "WarningMsg"}}, true, {}) -- default {history = true}
-                          if val == currFilePathElems[idx] then
-                              branchIdx = branchIdx + 1
+                              -- Split the paths into their directories.
+                              local destPathElems = splitPath(destPath)
+                              local currFilePathElems = splitPath(currFileParentDirPath)
+                              -- Identify the first elements of the path that are identical.
+                              local branchIdx = 0
+                              for idx, val in ipairs(destPathElems) do
+                                --vim.api.nvim_echo({{val, "WarningMsg"}}, true, {}) -- default {history = true}
+                                --vim.api.nvim_echo({{currFilePathElems[idx], "WarningMsg"}}, true, {}) -- default {history = true}
+                                if val == currFilePathElems[idx] then
+                                    branchIdx = branchIdx + 1
+                                    --vim.api.nvim_echo({{"branchIdx is "..branchIdx, "WarningMsg"}}, true, {}) -- default {history = true}
+                                  else
+                                    --vim.api.nvim_echo({{"breaking", "WarningMsg"}}, true, {}) -- default {history = true}
+                                    break
+                                end
+                              end
                               --vim.api.nvim_echo({{"branchIdx is "..branchIdx, "WarningMsg"}}, true, {}) -- default {history = true}
-                            else
-                              --vim.api.nvim_echo({{"breaking", "WarningMsg"}}, true, {}) -- default {history = true}
-                              break
-                          end
-                        end
-                        --vim.api.nvim_echo({{"branchIdx is "..branchIdx, "WarningMsg"}}, true, {}) -- default {history = true}
 
-                        -- When there are no identical elements, set the branchIdx to 1.
-                        -- Fix: Don't do this as it will break cases when there is no
-                        -- common root branch.
-                        -- if branchIdx == 0 then branchIdx = 1 end
+                              -- When there are no identical elements, set the branchIdx to 1.
+                              -- Fix: Don't do this as it will break cases when there is no
+                              -- common root branch.
+                              -- if branchIdx == 0 then branchIdx = 1 end
 
-                        --vim.api.nvim_echo({{"branchIdx is "..branchIdx, "WarningMsg"}}, true, {}) -- default {history = true}
-                        --vim.api.nvim_echo({{"Length of currFilePathElems is "..#currFilePathElems, "WarningMsg"}}, true, {}) -- default {history = true}
-                        --vim.api.nvim_echo({{"Length of destPathElems is "..#destPathElems, "WarningMsg"}}, true, {}) -- default {history = true}
+                              --vim.api.nvim_echo({{"branchIdx is "..branchIdx, "WarningMsg"}}, true, {}) -- default {history = true}
+                              --vim.api.nvim_echo({{"Length of currFilePathElems is "..#currFilePathElems, "WarningMsg"}}, true, {}) -- default {history = true}
+                              --vim.api.nvim_echo({{"Length of destPathElems is "..#destPathElems, "WarningMsg"}}, true, {}) -- default {history = true}
 
-                        -- For each directory level above the current file
-                        -- add ../ to the result path.
-                        local result = ""
-                        local relPath = ""
-                        -- If the destination anchor is in the current file, no
-                        -- paths need to be adjusted. That is, only do something if the
-                        -- destination anchor is in a different file (strip the file
-                        -- extension!).
-                        if destPath ~= vim.api.nvim_eval("expand('%:r')") then
-                          for currIdx = branchIdx + 1, #currFilePathElems do
-                            --vim.api.nvim_echo({{"currIdx is "..currIdx, "WarningMsg"}}, true, {}) -- default {history = true}
-                            relPath = relPath .. "../"
-                          end
-                          -- Add each directory level of the destination file.
-                          for destIdx = branchIdx + 1, #destPathElems do
-                            --vim.api.nvim_echo({{"destIdx is "..destIdx, "WarningMsg"}}, true, {}) -- default {history = true}
-                            relPath = relPath .. destPathElems[destIdx] .. "/"
-                          end
-                          -- Remove the trailing slash.
-                          if relPath:sub(-1) == "/" then relPath = relPath:sub(1,-2) end
-                        end
-                        -- Add the anchor.
-                        result = relPath .. "#" .. anchor
-                        return {result}
-                      end, {}),
+                              -- For each directory level above the current file
+                              -- add ../ to the result path.
+                              local result = ""
+                              local relPath = ""
+                              -- If the destination anchor is in the current file, no
+                              -- paths need to be adjusted. That is, only do something if the
+                              -- destination anchor is in a different file (strip the file
+                              -- extension!).
+                              if destPath ~= vim.api.nvim_eval("expand('%:r')") then
+                                for currIdx = branchIdx + 1, #currFilePathElems do
+                                  --vim.api.nvim_echo({{"currIdx is "..currIdx, "WarningMsg"}}, true, {}) -- default {history = true}
+                                  relPath = relPath .. "../"
+                                end
+                                -- Add each directory level of the destination file.
+                                for destIdx = branchIdx + 1, #destPathElems do
+                                  --vim.api.nvim_echo({{"destIdx is "..destIdx, "WarningMsg"}}, true, {}) -- default {history = true}
+                                  relPath = relPath .. destPathElems[destIdx] .. "/"
+                                end
+                                -- Remove the trailing slash.
+                                if relPath:sub(-1) == "/" then relPath = relPath:sub(1,-2) end
+                              end
+                              -- Add the anchor.
+                              result = relPath .. "#" .. anchor
+                              return {result}
+                            end, {}),
 
-                    -- Last Placeholder, exit Point of the snippet. EVERY 'outer' SNIPPET NEEDS Placeholder 0.
-                    i(0),
-                    t({")"}),
-            })
-    }
-}
+                          -- Last Placeholder, exit Point of the snippet. EVERY 'outer' SNIPPET NEEDS Placeholder 0.
+                          i(0),
+                          t({")"}),
+                  })
+                }
+)
 
 --return m
