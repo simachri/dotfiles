3b4c7df77bcb536bd79aaceb7c90529dbd1192b7
simachri <marvin.christe@posteo.de>
2022-09-21 10:13:51 +0200

    Some changes.

--- .config/nvim/after/ftplugin/confluencewiki.vim
+++ .config/nvim/after/ftplugin/confluencewiki.vim
@@ -11,3 +11,11 @@ set wrap
 set tw=0
 " Disable wrap marging as this will insert line breaks.
 set wrapmargin=0
+
+" Make text italic.
+nmap <buffer><silent> <leader>mi ysiW_
+" Make text bold.
+nmap <buffer><silent> <leader>mb ysiW*
+" Make text code.
+nmap <buffer><silent> <leader>mc ysiW}l.
+
--- .config/nvim/lua/plugin/git.lua
+++ .config/nvim/lua/plugin/git.lua
@@ -320,7 +320,7 @@ require('gitsigns').setup {
 
     -- Actions
     --map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    --map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map({'n', 'v'}, '<leader>vr', ':Gitsigns reset_hunk<CR>')
     --map('n', '<leader>hS', gs.stage_buffer)
     --map('n', '<leader>hu', gs.undo_stage_hunk)
     --map('n', '<leader>hR', gs.reset_buffer)
