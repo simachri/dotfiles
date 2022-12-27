1abff3c2e365ccc5ef7c154e67286e877602c0da
simachri <marvin.christe@posteo.de>
2022-12-27 10:04:41 +0100

    Auto-Session: Automatically load session upon Vim start.

--- .config/nvim/after/plugin/auto-session.lua
+++ .config/nvim/after/plugin/auto-session.lua
@@ -1,20 +1,15 @@
---require("persistence").setup({
-  --dir = vim.fn.expand(vim.fn.stdpath("config") .. "/sessions/"), -- directory where session files are saved
-  --options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
---})
---vim.api.nvim_set_keymap("n", "<Leader>sr", [[<cmd>lua require("persistence").load()<cr>]], {})
-
 -- https://github.com/rmagatti/auto-session
 -- 21-07-31: The key remapping is required to suppress
 -- a 'bwipe 1' error when restoring a session.
+-- 22-12-27: Seems to be fixed.
 vim.api.nvim_set_keymap("n", "<Leader>sr", "<cmd>silent! RestoreSession<CR>", {})
 local opts = {
-  log_level = 'info',
+  log_level = 'error',
   auto_session_enable_last_session = false,
   auto_session_root_dir = "/home/xi3k/.config/nvim/sessions/",
   auto_session_enabled = true,
   auto_save_enabled = true,
-  auto_restore_enabled = false,
+  auto_restore_enabled = true,
   auto_session_suppress_dirs = {'~/.config/nvim/plugged/*'},
   -- For some reason, scrolloff is set to 0 when a session is restored.
   post_restore_cmds = {"set scrolloff=8"},
