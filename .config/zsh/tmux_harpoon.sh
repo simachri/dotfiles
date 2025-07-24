aaaad3d35e989234a1dc73bd17305d7ec368ae49
simachri <marvin.christe@posteo.de>
2025-07-24 13:11:15 +0200

    some changes

--- .config/zsh/tmux_harpoon.sh
+++ .config/zsh/tmux_harpoon.sh
@@ -30,19 +30,6 @@ harpoon_add() {
     echo "$entry" >> "$HARPOON_FILE"
 }
 
-harpoon_remove() {
-    init_harpoon
-    local line_num="$1"
-    
-    if [ -z "$line_num" ]; then
-        echo "Usage: harpoon_remove <line_number>"
-        return 1
-    fi
-    
-    sed -i "${line_num}d" "$HARPOON_FILE"
-    echo "Removed harpoon entry #$line_num"
-}
-
 # Navigate to harpoon entry by number (1-indexed)
 harpoon_nav() {
     init_harpoon
@@ -73,63 +60,4 @@ harpoon_nav() {
     else
         tmux attach-session -t "${session}:${window_index}"
     fi
-}
-
-harpoon_list() {
-    init_harpoon
-    
-    if [ ! -s "$HARPOON_FILE" ]; then
-        echo "No harpoon entries"
-        return 0
-    fi
-    
-    echo "=== Tmux Harpoon Entries ==="
-    cat -n "$HARPOON_FILE"
-}
-
-harpoon_clear() {
-    init_harpoon
-    > "$HARPOON_FILE"
-    echo "Cleared all harpoon entries"
-}
-
-# Main harpoon function - acts as dispatcher
-harpoon() {
-    case "$1" in
-        "add"|"a")
-            harpoon_add
-            ;;
-        "remove"|"rm"|"r")
-            harpoon_remove "$2"
-            ;;
-        "nav"|"n")
-            harpoon_nav "$2"
-            ;;
-        "list"|"ls"|"l")
-            harpoon_list
-            ;;
-        "clear"|"c")
-            harpoon_clear
-            ;;
-        "clean")
-            harpoon_clean
-            ;;
-        "help"|"h"|"--help")
-            cat << EOF
-Tmux Harpoon - Quick session/window navigation
-
-Commands:
-  harpoon add     - Add current tmux location to harpoon
-  harpoon nav <n> - Navigate to harpoon entry number n
-  harpoon list    - List all harpoon entries
-  harpoon remove <n> - Remove harpoon entry number n
-  harpoon clear   - Clear all harpoon entries
-EOF
-            ;;
-        *)
-            echo "Unknown command: $1"
-            echo "Use 'harpoon help' for usage information"
-            return 1
-            ;;
-    esac
 }
